3b43ba7401498fae257bfb1f7088d2ddd4ff955f
Jens Windmüller <jens.windmueller@swplusplus.de>
2019-05-22 17:18:23 +0200

    delete no longer used bootstrapping

--- backend/src/test/java/de/swplusplus/gamereleaseview/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/swplusplus/gamereleaseview/backend/BackendApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BackendApplicationTests {
